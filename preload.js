9b39e1483c125887a66bd78ce6bedb12cb363095
D.H <60787559+cakecandy@users.noreply.github.com>
2022-10-23 19:07:29 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,8 +1,13 @@
 // A BrowserWindow's preload script runs in a context that has access to both the HTML DOM 
 // and a limited subset of Node.js and Electron APIs
+/*
+ * The contextBridge allows us to inject/add a key (in our case, "api") on the window, 
+ * with an object where we can define any number of properties that are available within the renderer process.
+*/
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('darkMode', {
-  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
-  system: () => ipcRenderer.invoke('dark-mode:system')
+contextBridge.exposeInMainWorld('api', {
+  // test
+  openFile: () => ipcRenderer.invoke('dialog:openFile')
+  
 })
