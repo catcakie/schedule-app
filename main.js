f23da919f8b3eace1458126ef3a442a8a3bdf94a
D.H <60787559+cakecandy@users.noreply.github.com>
2022-11-10 10:39:05 -0500

    edit comments

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // commands
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
+// Grab all the command files from the commands directory
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
@@ -126,7 +126,7 @@ for (const file of commandFiles) {
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(token);
 
-// and deploy your commands!
+// deploy the commands
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
@@ -139,7 +139,7 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
+		// make sure to catch and log any errors
 		console.error(error);
 	}
 })();
