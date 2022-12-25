082b769167e2e8812b134b72d8e0cc4ea383d207
D.H <60787559+cakecandy@users.noreply.github.com>
2022-12-25 12:04:08 -0500

    removed category 'Nutrition'

--- renderer.js
+++ renderer.js
@@ -290,7 +290,7 @@ let config = {
 			resizable: false,
 			editable: {
 				type: 'combo',
-				items: ['Journal', 'Mindset', 'Info', 'Generosity', 'Care-taking', 'Hobby', 'Appearance', 'Work', 'Deadline', 'School', 'Nutrition', 'Break']
+				items: ['Journal', 'Mindset', 'Info', 'Generosity', 'Care-taking', 'Hobby', 'Appearance', 'Work', 'Deadline', 'School', 'Break']
 			}
 		},
 		{
