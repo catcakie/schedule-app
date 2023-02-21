8c15f219b52caeb2a2c3d308ccebda947efc5d96
D.H <60787559+cakecandy@users.noreply.github.com>
2023-02-20 20:57:39 -0500

    removed 14k link & added space to fur keyword

--- main.js
+++ main.js
@@ -281,7 +281,7 @@ const cheerio = require("cheerio")
 const puppeteer = require("puppeteer")
 const YAML = require('yaml')
 
-const keywords = ["3.75", "Spinel", "Platinum", "Genuine Leather", "Fur", "14K", "14k", "10K", "Louis Vuitton", "Dior", "Chanel", "Tiffany", "Prada", "Celine", "Hermes", "Gucci"]
+const keywords = ["3.75", "Spinel", "Platinum", "Genuine Leather", "Fur ", "14K", "14k", "10K", "Louis Vuitton", "Dior", "Chanel", "Tiffany", "Prada", "Celine", "Hermes", "Gucci"]
 let linkCache = []
 
 const goodwillNewJewelryLink = "https://www.goodwillfinds.com/jewelry/rings/?sz=5000"
@@ -340,12 +340,10 @@ setInterval(async () => {
 */
 notifyShopGoodwillItems(shopgoodwillJewelryLink)
 notifyShopGoodwillItems(shopgoodwillFurLink)
-notifyShopGoodwillItems(shopgoodwill14kLink)
 
 setInterval(async () => {
     notifyShopGoodwillItems(shopgoodwillJewelryLink)
     notifyShopGoodwillItems(shopgoodwillFurLink)
-    notifyShopGoodwillItems(shopgoodwill14kLink)
   }, 60000)
 
 function notifyShopGoodwillItems(link) {
