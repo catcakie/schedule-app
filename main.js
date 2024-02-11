cccc8122a70d4f4cf8f2562123411a3ad1815626
Danielle <60787559+catcakie@users.noreply.github.com>
2024-02-11 17:40:51 -0500

    updated fb marketplace selector from img.src to href

--- main.js
+++ main.js
@@ -401,7 +401,7 @@ function getFacebookMarketplaceImages(link) {
         await page.goto(link);
         await page.waitForSelector('.xt7dq6l.xl1xv1r.x6ikm8r.x10wlt62.xh8yej3')
 
-        const pageContent = await page.$$eval('.xt7dq6l.xl1xv1r.x6ikm8r.x10wlt62.xh8yej3', items => {return items.map(item => item.src)} )
+        const pageContent = await page.$$eval('.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.x1lku1pv', items => {return items.map(item => item.href)} )
 
         //pageContent.forEach(item => console.log(item))
         pageContent.forEach(item => client.channels.cache.get(`1077663232564678686`).send(item))
